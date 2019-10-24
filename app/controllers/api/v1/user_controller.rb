baf68d72c8ef628b7e28b4202c510c2e54ceec7d
mrh-jishan <ra.onerobin@gmail.com>
2019-10-25 00:19:12 +0800

    remove user password from response

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UserController < ApplicationController
     def create
         @user = User.new(user_param)
         if @user.save
-            render :json => {:success=> true, :user=> @user}
+            render :json => {:success=> true, :user=> @user.as_json(except: [:password_digest])}
         else
             render :json => {:success=> false, :message=> @user.errors.full_messages}
         end
