5d38307cb2ae322eed1df830baf97d7fc0728947
robin-hassan <robiul.hassan12102@gmail.com>
2020-01-26 01:34:44 +0800

    added kitchen addedress to kitchen response

--- app/controllers/api/v1/kitchen_controller.rb
+++ app/controllers/api/v1/kitchen_controller.rb
@@ -2,13 +2,8 @@ class Api::V1::KitchenController < ApplicationController
   before_action :authorize_request
 
   def index
-    kitchens = Kitchen.joins([:tags, :user]).includes([:tags, :user])
-    json_response({kitchens: kitchens.as_json(:include => [:tags, :user => {:except => [:password_digest, :confirm_token]}])}, 200)
+    kitchens = Kitchen.joins([:tags, :user, :kitchen_address]).includes([:tags, :user, :kitchen_address])
+    json_response({kitchens: kitchens.as_json(:include => [:tags, :kitchen_address, :user => {:except => [:password_digest, :confirm_token]}])}, 200)
   end
 
-  protected
-
-  def kitchen_param
-    params.required(:kitchen).permit(:name, :title, :description, tags_attributes: [:name])
-  end
 end
